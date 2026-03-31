5a5b6bc6cab02bfdd0bc09ceb7e2ea441a02b2b6
User Oksana <oks321mih@yandex.ru>
2026-03-31 22:28:40 +0300

    Checkstyle

--- ewm-main-service/src/main/java/ru/practicum/main/event/service/EventServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/main/event/service/EventServiceImpl.java
@@ -5,7 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.main.category.model.Category;
@@ -28,7 +27,6 @@ import jakarta.servlet.http.HttpServletRequest;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 import java.util.Objects;
 import java.util.stream.Collectors;
