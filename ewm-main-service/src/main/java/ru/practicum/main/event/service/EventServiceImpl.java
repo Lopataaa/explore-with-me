03fab6fd4187cd67508f9834e624b7a3894842e1
User Oksana <oks321mih@yandex.ru>
2026-04-02 22:19:59 +0300

    Проверка прохождения тестов

--- ewm-main-service/src/main/java/ru/practicum/main/event/service/EventServiceImpl.java
+++ ewm-main-service/src/main/java/ru/practicum/main/event/service/EventServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.practicum.main.event.service;
 
+// Проверка прохождения тестов
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Page;
